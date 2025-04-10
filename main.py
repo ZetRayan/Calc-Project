2e8a88328c0c87e2b02e9671d7e063e77e55b3c6
ZetRayan <60727901+ZetRayan@users.noreply.github.com>
2025-04-10 15:42:48 +0300

    Update main.py
    Модификация скрипта для чисел от 0 до 10 вместо от 0 до 9

--- main.py
+++ main.py
@@ -4,19 +4,41 @@ from math import floor
 def main(input: str):
     try:
         input = input.replace(" ", "")
+        operators = ['+', '-', '*', '/']
         
-        if len(input)>3: 
-            raise ValueError("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)! Операнды должны быть целыми числами от 0 до 9") 
         if len(input)<3: 
             raise ValueError("Строка не является математической операцией")
         
-        a,operator,b = input
-        if (a.isdigit() and b.isdigit()) == False:
-            raise ValueError("Операнды должны быть целыми числами от 0 до 9")
-        if operator not in ['+', '-', '*', '/']:
-            raise ValueError("Оператор должен быть одним из: +, -, *, /")
+        operator = ''
+        operator_index = -1
         
-        a,b = int(a) , int(b)
+        for _operator in operators:
+            for index, char in enumerate(input):
+                if char == _operator:
+                    if operator == '':
+                        operator = _operator
+                        operator_index = index
+                    else:
+                        raise ValueError("В выражении больше одного оператора!")
+                    
+        if operator_index == -1:
+            raise ValueError("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)! Операнды должны быть целыми числами от 0 до 10") 
+        
+        a = input[0:operator_index]
+        b = input[operator_index+1:]
+        
+        if a == '' or b == '':
+            raise ValueError("Отсутствует од из апперандов!")
+        
+        try:
+            a = int(a)
+            b = int(input[operator_index+1:])
+        except ValueError:
+            raise ValueError(f"Операнды должны быть целыми числами. {a} и {b} не являются целыми числами!")
+        if a > 10 or b > 10:
+            raise ValueError("Операнды должны быть целыми числами от 0 до 10")
+            
+
         if operator == '/' and b == 0: 
             raise ZeroDivisionError ("На 0 делить нельзя!")
         
@@ -32,7 +54,6 @@ def main(input: str):
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         exit()
-
 
 result = main(input("Input:\n"))
 print (f"Output:\n{floor(result)}") 
